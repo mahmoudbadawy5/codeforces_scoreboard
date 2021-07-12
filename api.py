0a81ebaf8e88e06b07ec783adf1fe8ae452198ff
mahmoudbadawy5 <mahmoudbadawy2011@gmail.com>
2021-07-12 06:05:43 +0200

    Implemented scoreboard generator

---
+++ api.py
@@ -0,0 +1,36 @@
+# Local imports
+import config
+
+
+# Global imports 
+import time, os
+import random
+import hashlib
+import urllib.request
+import json
+
+
+def getRequest(method,data):
+	rnd=random.randint(10**5,10**6-1);
+	data["time"] = int(time.time())
+	data["apiKey"]= config.apiKey
+	params=""
+	isFirst=True
+	keys = data.keys()
+	keys = sorted(keys)
+	for key in keys:
+		if(isFirst == False):
+			params=params+"&"
+		isFirst=False
+		params=params+str(key)+"="+str(data[key])
+	sigData=str(rnd)+"/"+method+"?"+params+"#"+config.apiSecret
+	sigData=hashlib.sha512(sigData.encode('utf-8')).hexdigest()
+	return "https://codeforces.com/api/"+method+"?"+params+"&apiSig="+str(rnd)+sigData
+
+def callApi(method,data):
+	req = getRequest(method,data)
+	response = urllib.request.urlopen(req).read()
+	result = json.loads(response)
+	if result['status'] != u'OK':
+		return None
+	return result
