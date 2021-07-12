3b2a52c39c0a878e517c780576458d44f9533953
mahmoudbadawy5 <mahmoudbadawy2011@gmail.com>
2021-07-12 04:59:20 +0200

    Fixed again

--- contest-17325/problems/keys-and-lamps-codeforces/wipe.bat
+++
@@ -1,7 +0,0 @@
-rem    *** samples ***
-del samples\01.a
-
-rem    *** tests ***
-del tests\01
-del tests\01.a
-
--- contest-17325/problems/keys-and-lamps-codeforces/wipe.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-#   *** samples ***
-rm -f samples/01.a
-
-#   *** tests ***
-rm -f tests/01
-rm -f tests/01.a
-
